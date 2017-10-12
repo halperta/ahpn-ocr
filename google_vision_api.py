427825f43622d3cf1bd0ca68523ce450721211d3
Hannah Alpert-Abrams <halperta@gmail.com>
2017-10-12 11:53:24 -0500

    updating vision codes

--- google_vision_api.py
+++ google_vision_api.py
@@ -16,6 +16,7 @@ import sys
 import io
 import os
 import glob
+import codecs
 
 # Imports the Google Cloud client library
 from google.cloud import vision
@@ -24,52 +25,71 @@ from google.cloud.vision import types
 # Instantiates a client
 client = vision.ImageAnnotatorClient()
 
-def ocrFiles(imageDirectoryPath, outputPath):
-	filePaths_list = glob.glob("%(imageDirectoryPath)s/*.jpg" % {"imageDirectoryPath": imageDirectoryPath}) 
-	for filePath in filePaths_list:
-		fileName = os.path.basename(filePath)
+def make_request_from_file(filepath):
+  # Loads the image into memory
+  with io.open(filepath, 'rb') as image_file:
+    content = image_file.read()
+  image = types.Image(content=content)
 
-		# Loads the image into memory
-		with io.open(filePath, 'rb') as image_file:
-			content = image_file.read()
-		image = types.Image(content=content)
+  # response = client.document_text_detection(image=image)
+  request = dict(
+    image = image,
+    features = [
+      dict(type='DOCUMENT_TEXT_DETECTION'),
+      # Add more entries here if more types are desired.
+    ],
+    image_context = dict(
+      language_hints=['es']  # optional
+    ),
+  )
+  return request
 
-		response = client.document_text_detection(image=image)
 
-		text_annotations = response.text_annotations
+def ocrFiles(imageDirectoryPath, outputPath, max_images):
+  requests = []
+  filenames = []
+  filepaths_list = glob.glob("%(imageDirectoryPath)s/*.jpg" % {"imageDirectoryPath": imageDirectoryPath}) 
+  for filepath in filepaths_list[:max_images]:
+    request = make_request_from_file(filepath)
+    requests.append(request)
+    filenames.append(os.path.basename(filepath))
 
-		f = open('%(outputPath)s/%(fileName)s-ocr.txt' % {"outputPath": outputPath,"fileName": fileName},'w')
-		for entity_annotation in text_annotations:
-		  f.write("language: ", entity_annotation.locale)
-		  f.write("text:\n", entity_annotation.description)
-		f.close()
+  batch_response = client.batch_annotate_images(requests)
+  print("responses.size = ", sum(1 for _ in batch_response.responses))
+  for (response, filename) in zip(batch_response.responses, filenames):
+    text_annotations = response.text_annotations
+
+    with codecs.open('%(outputPath)s/%(filename)s-ocr.txt' % {"outputPath": outputPath,"filename": filename},'w',"utf-8") as f:
+      for entity_annotation in text_annotations:
+        f.write('language: %(language)s\n' % {"language": entity_annotation.locale})
+        f.write("text:\n %(text)s\n" % {"text": entity_annotation.description})
 
 if __name__ == '__main__': 
 
   (imageDirectoryPath, outputPath) = sys.argv[1:]
-  ocrFiles(imageDirectoryPath, outputPath)  
+  ocrFiles(imageDirectoryPath, outputPath, max_images = 16)
 
 
 # The name of the image file to annotate
-# file_name = os.path.join(
+# filename = os.path.join(
 #     os.path.dirname(__file__),
 #     'cirma_inforpress_0079_002.jpg')
 
 #haa 9-26: create list of all files and filepaths
 
-# def getAllFileNames(directoryPath, fileExtension):
-# 	filePaths_list = glob.glob("%(directoryPath)s/*.jpg" % {"directoryPath": directoryPath}) 
-# 	fileNames_list = list()
-# 	for filePath in filePaths_list:
-# 	  fileNameWithExt = os.path.basename(filePath)
-# 	  fileName = fileNameWithExt.replace(fileExtension,"")
-# 	  fileNames_list.append(fileName)
-# 	return fileNames_list, filePaths_list
+# def getAllfilenames(directoryPath, fileExtension):
+#   filepaths_list = glob.glob("%(directoryPath)s/*.jpg" % {"directoryPath": directoryPath}) 
+#   filenames_list = list()
+#   for filepath in filepaths_list:
+#     filenameWithExt = os.path.basename(filepath)
+#     filename = filenameWithExt.replace(fileExtension,"")
+#     filenames_list.append(filename)
+#   return filenames_list, filepaths_list
 
 #haa 9-26: modified to create capacity for multiple files.
-# file_path_root = '/Users/halperta/GitHub/ahpn-ocr/'
-# file_name = 'cirma_inforpress_0079_002.jpg'
-# file_path = file_path_root + file_name
+# filepath_root = '/Users/halperta/GitHub/ahpn-ocr/'
+# filename = 'cirma_inforpress_0079_002.jpg'
+# filepath = filepath_root + filename
 
 # Performs label detection on the image file
 # response = client.label_detection(image=image)
